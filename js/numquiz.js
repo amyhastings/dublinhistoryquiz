bceb0544f045ac569ccf1faf2a8e1dbf41f8361a
Amy Hastings <amy.hastings@gmail.com>
2024-07-18 13:53:33 +0100

    Numbers quiz validation (including modal error messages)

--- js/numquiz.js
+++ js/numquiz.js
@@ -1,38 +1,49 @@
 const numQuizData = [
     { 
         id: "numq1",
-        question: "What year did the Battle of Clontarf take place?", 
+        question: "Q1. What year did the Battle of Clontarf take place?", 
         answer: "1014",
+        answertype: "year",
+        errormsg: "For Question 1, please input a valid year. This event took place after 0 AD/CE.",
         fact: "xxxx",
     },
     { 
         id: "numq2",
-        question: "How many Cathedrals are there in Dublin?", 
+        question: "Q2. How many Cathedrals are there in Dublin?", 
         answer: "2",
+        answertype: "quantity",
+        errormsg: "For Question 2, please enter a valid quantity (e.g. 0 or greater).",
         fact: "xxxx",
     },
     { 
         id: "numq3",
-        question: "What year did the Act of Union come into force?", 
+        question: "Q3. What year did the Act of Union come into force?", 
         answer: "1801",
+        answertype: "year",
+        errormsg: "For Question 3, please input a valid year. This event took place after 0 AD/CE.",
         fact: "xxxx",
     },
     { 
         id: "numq4",
-        question: "What year did the Easter Rising take place?", 
+        question: "Q4. What year did the Easter Rising take place?", 
         answer: "1916",
+        answertype: "year",
+        errormsg: "For Question 4, please input a valid year. This event took place after 0 AD/CE.",
         fact: "xxxx",
     },
     { 
         id: "numq5",
-        question: "How tall (in metres) is the Spire?", 
+        question: "Q5. How tall (in metres) is the Spire?", 
         answer: "120",
+        answertype: "quantity",
+        errormsg: "For Question 5, please enter a valid quantity (e.g. 0 or greater).",
         fact: "At 120 metres tall, the Spire is taller than the Statue of Liberty in New York.",
     },
 ];
 
-let numCurrentQuestionIndex = 0;
-let numQuizCorrect = 0;
+let modal = document.getElementById("errorModal");
+let span = document.getElementsByClassName("close")[0];
+
 
 const formContainer = document.querySelector(".numquiz-form");
 const formContent = document.querySelector("#numquiz-form-content");
@@ -54,45 +65,67 @@ function generateForm() {
     formContent.innerHTML = newFormContent;
 }
 
-// function attachFormSubmitListener() {
-//     let formElem = document.getElementById("numquiz-form");
-//     formElem.addEventListener('submit', event => {
-//       event.preventDefault();  
-//     });
-//     checkAnswer();
-//     formContainer.classList.add("inactive");
-//     numResultsContainer.classList.remove("inactive");
-//     numTotalSpan.textContent = numQuizData.length;
-//     numCorrectSpan.textContent = numQuizCorrect;
-// }
-
 function checkNumAnswer() {
+    let numQuizCorrect = 0;
     let formElem = document.getElementById("numquiz-form");
+    let formIncomplete = false;
+    let formInvalidYear = false;
+    let formInvalidQuantity = false;
+    let errormsgText = "";
+    let modalTextContainer = document.getElementById("modal-text");
     formElem.addEventListener('submit', event => {
       event.preventDefault();  
     });
-    numQuizData.forEach(numCurrentQuestion => {
-        let answerField = document.getElementById(`${numCurrentQuestion.id}`);
+    numQuizData.forEach(currentQuestion => {
+        let answerField = document.getElementById(`${currentQuestion.id}`);
         let answerValue = answerField.value;
+        let numCurrentQuestion = numQuizData[currentQuestionIndex];
+        if (answerValue === "") { 
+            formIncomplete = true;
+        } 
+        if (numCurrentQuestion.answertype === "year" && answerValue < 0 || numCurrentQuestion.answertype === "year" && answerValue > 2024) { 
+            formInvalidYear = true;
+            errormsgText += `<p>${currentQuestion.errormsg}</p>`;
+        } 
+        if (numCurrentQuestion.answertype === "quantity" && answerValue < 0) {
+            formInvalidQuantity = true;
+            errormsgText += `<p>${currentQuestion.errormsg}</p>`;
+        }
         if (numCurrentQuestion.answer === answerValue) {
             numQuizCorrect++;
-        };
+        }
+    });
+    if (formIncomplete) {
+        errormsgText += "Please fill in all fields. If you are not sure of an answer, take a guess!";
     }
-    );
-    formContainer.classList.add("inactive");
-    numResultsContainer.classList.remove("inactive");
-    numTotalSpan.textContent = numQuizData.length;
-    numCorrectSpan.textContent = numQuizCorrect; 
+    if (formIncomplete === true  || formInvalidYear === true || formInvalidQuantity === true) {
+        modal.style.display = "block";
+        modalTextContainer.innerHTML = `${errormsgText}`;
+    } else {
+        modal.style.display = "none";       
+        formContainer.classList.add("inactive");
+        numResultsContainer.classList.remove("inactive");
+        numTotalSpan.textContent = numQuizData.length;
+        numCorrectSpan.textContent = numQuizCorrect;
+    }
 };
+
 
 document.addEventListener('click', function(e){
     const id = e.target.id;
     if (id === "numrestart") {
         formContainer.classList.remove("inactive");
         numResultsContainer.classList.add("inactive");
-        numQuizCorrect = 0;
         formContainer.reset();
     } else if (id === "guess") {
         checkNumAnswer();
+    } else if (id === "closemodal") {
+        modal.style.display = "none";
     }
 });
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
